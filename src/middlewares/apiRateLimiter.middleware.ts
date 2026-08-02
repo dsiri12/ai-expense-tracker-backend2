7d1ea7933f7ec3e29dbb0ea481db8c893fc49b4a
Damien Siri <dsiri12@gmail.com>
2026-08-02 14:20:49 -0400

    api rate limit

--- src/middlewares/apiRateLimiter.middleware.ts
+++ src/middlewares/apiRateLimiter.middleware.ts
@@ -3,6 +3,7 @@ import rateLimit from "express-rate-limit";
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes window
   max: 100, // Limit each IP to 100 requests per windowMs
+  // ratelimit-policy	100;w=900
   message: {
     status: 429,
     error: 'Too many requests, please try again later.'
