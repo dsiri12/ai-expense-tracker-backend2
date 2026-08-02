71ea7e9ed1e3c31bc3de79e70c39c399f95ecf9b
Damien Siri <dsiri12@gmail.com>
2026-08-01 20:38:06 -0400

    setup the api rate limit

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import cors from 'cors';
 import authRoutes from './features/auth/auth.routes';
 import categoryRoutes from './features/category/category.routes'
 import { errorHandler } from './common/errors/errorHandler';
+import { apiLimiter } from './middlewares/apiRateLimiter.middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/', apiLimiter);
 
 app.get('/ping', (req: Request, res: Response) => {
    res.json({ message: 'AI Expense Tracker API is running' });
